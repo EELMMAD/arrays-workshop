cf9f10746e584d50887d88ee1f659bf0611f7e63
Elmira Madadi <elmiramadadi@gmail.com>
2020-09-24 09:52:31 +0200

    Uploaded first version

---
+++ src/main/java/se/lexicon/elmira/App.java
@@ -0,0 +1,9 @@
+package se.lexicon.elmira;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/se/lexicon/elmira/data/NamesStorage.java
@@ -0,0 +1,7 @@
+package se.lexicon.elmira.data;
+
+public class NamesStorage {
+
+    private static String[] names = new String[0];
+    
+}

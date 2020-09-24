6a62be07b731a808ff132c60266c12ab281f36c7
Elmira Madadi <elmiramadadi@gmail.com>
2020-09-24 11:59:52 +0200

    Added nameExists() and add() in NamesStorage.java

--- src/main/java/se/lexicon/elmira/data/NamesStorage.java
+++ src/main/java/se/lexicon/elmira/data/NamesStorage.java
@@ -1,13 +1,48 @@
 package se.lexicon.elmira.data;
 
-public class NamesStorage {
+import java.util.Arrays;
+
+public class NamesStorage { //class starts
 
     private static String[] names = new String[0];
 
+    /**
+     *
+     * @param fullName String fullName
+     * @return true if fount
+     */
+    public static boolean nameExists(final String fullName){  //methods starts
+        boolean exists = false;
+       for (String name : names){    //for Loop starts
+           if(name.equalsIgnoreCase(fullName)){   //if statement starts
+               exists = true;
+               break;     //Breaks out of a Loop or a switch.
+           }  //if statement ends
+       }//for Loops ends
+        return exists;
+    }// method ends
 
+    public static boolean add(final String fullName){
+     if(fullName == null){
+         return false;
+     }
+     if(nameExists(fullName)){
+         return false;
+     }
+     names = Arrays.copyOf(names, names.length + 1);
+     names[names.length - 1] = fullName;
+     return true;
+    }
+
+
+
+
+    /**
+     * Empties the array by overwriting it with a new empty array
+     */
     public static void clear(){
         names = new String[0];
     }
-}
+}// class ends
 
 

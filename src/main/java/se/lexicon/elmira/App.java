d418768d0aff43ca1622d8a93e596d9b1c8300bc
Elmira Madadi <elmiramadadi@gmail.com>
2020-09-24 13:46:47 +0200

    Add a comment

--- src/main/java/se/lexicon/elmira/App.java
+++ src/main/java/se/lexicon/elmira/App.java
@@ -1,9 +1,22 @@
 package se.lexicon.elmira;
 
-public class App 
+import se.lexicon.elmira.data.NamesStorage;
+
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       boolean added = NamesStorage.add("Erik Svensson");
+        if(added){
+            System.out.println("A name was added");
+        }
+
+        added = NamesStorage.add("Erik Svensson");
+        if(added){
+            System.out.println("A name was added");
+        }else {
+            System.out.println("Name already exists");
+        }
+        System.out.println(NamesStorage.add(null));
     }
 }
